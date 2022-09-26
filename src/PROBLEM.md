d3de338f0cd97491a97e4648feb4dd959f8af655
HP <mdzaid1299@gmail.com>
2022-09-26 23:15:36 +0530

    initial

---
+++ src/PROBLEM.md
@@ -0,0 +1,2 @@
+Write a program to calculate the sum of the first and the second last digit of a 5 digit.
+E.g.- NUMBER : 12345        OUTPUT : 1+4=5
--- src/oprators.java
+++ src/oprators.java
@@ -1,5 +1,18 @@
 public class oprators {
     public static void main(String[] args) {
+        int n, sum,first, fourth, fifth, second;
+        n = 12345;
+        first = n / 10000;
+        n = n % 10000;
+//        second = n / 10000;
+//        n = n % 10000;
+        fourth = n / 1000;
+        n = n % 1000;
+//        fifth = n / 10000;
+//        n = n % 10000;
 
+        sum = first + fourth;
+        System.out.println("sum = " + sum);
     }
+
 }
